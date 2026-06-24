95b5cc677f8391d1d46366054229ec51e40f79c7
Alexander Abbasov <abbasovalex@gmail.com>
2026-06-24 14:57:51 +0300

    fix: use Playwright CDP connection in example

--- docs/code_examples/puppeteer_cdp/javascript/playwright_cdp_connect.js
+++ docs/code_examples/puppeteer_cdp/javascript/playwright_cdp_connect.js
@@ -1,11 +1,12 @@
 import { chromium } from 'playwright';
 
-const browser = await chromium.connect({
-    wsEndpoint: 'wss://cdp.xclouds.dev/cdp/?api_key=YOUR_API_KEY',
-});
+const browser = await chromium.connectOverCDP(
+    'wss://cdp.xclouds.dev/cdp/?api_key=YOUR_API_KEY',
+);
 
-const page = await browser.newPage();
-const client = await page.context().newCDPSession(page);
+const context = browser.contexts()[0];
+const page = await context.newPage();
+const client = await context.newCDPSession(page);
 await client.send('Animation.enable');
 client.on('Animation.animationCreated', () => console.log('Animation created!'));
 const response = await client.send('Animation.getPlaybackRate');
@@ -13,3 +14,4 @@ console.log('playback rate is ' + response.playbackRate);
 await client.send('Animation.setPlaybackRate', {
   playbackRate: response.playbackRate / 2
 });
+await browser.close();
